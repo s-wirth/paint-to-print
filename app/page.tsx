90e717cb48aea883b7af7991a407450a8957a373
Sophie Wirth <sophiewirth@macbook-pro.speedport.ip>
2025-06-18 10:58:02 +0200

    basic layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
+import styles from "./page.module.css";
+
 export default function Home() {
   const [uploadedImages, setUploadedImages] = useState([]);
   const [contouredImages, setContouredImages] = useState([]);
   const [fileToUpload, setFileToUpload] = useState(null);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedContouredFile, setSelectedContouredFile] = useState(null);
   const [responseData, setResponseData] = useState({
     message: "",
     fileName: "",
@@ -25,6 +28,9 @@ export default function Home() {
     const data = await res.json();
     console.log('data', data)
     setContouredImages(data["files"]);
+    if (data["files"].length > 0) {
+      setSelectedContouredFile(data["files"][0])
+    }
   }
 
   useEffect(() => {
@@ -78,40 +84,54 @@ export default function Home() {
   console.log('typeof selectedFile', typeof selectedFile)
   console.log('selectedFile', selectedFile)
   return (
-    <main>
-      <h1>Paint to Print</h1>
-      <h2>Uploaded Images</h2>
-      {uploadedImages.length > 0 && uploadedImages.map((image) => (
-        <Image
-          key={image}
-          src={`/uploads/${image}`}
-          alt="Uploaded Image"
-          width={200}
-          height={200}
-          className={selectedFile === image ? "uploadedImage selected" : "uploadedImage"}
-          onClick={() => setSelectedFile(image)}
-        />
-      ))}
-      {selectedFile && <button onClick={getContour}>Get Contour</button>}
-      <h2>Contoured Images</h2>
-      {contouredImages.length > 0 && contouredImages.map((image) => (
-        <Image
-          key={image}
-          src={`/opencv_store/${image}`}
-          alt="Uploaded Image"
-          width={200}
-          height={200}
-          className={selectedFile === image ? "uploadedImage selected" : "uploadedImage"}
-          onClick={() => setSelectedFile(image)}
-        />
-      ))}
-      {responseData.message && <p>{responseData.message}</p>}
-      {responseData.fileName && <p>{responseData.fileName}</p>}
-      <h2>Upload an Image</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="file" onChange={handleFileChange} />
-        <button type="submit">Upload</button>
-      </form>
+    <main className={styles.main}>
+      <div className={styles.fileSelection}>
+        <h1>Paint to Print</h1>
+        <h2>Uploaded Images</h2>
+        {uploadedImages.length > 0 && uploadedImages.map((image) => (
+          <Image
+            key={image}
+            src={`/uploads/${image}`}
+            alt="Uploaded Image"
+            width={200}
+            height={200}
+            className={selectedFile === image ? styles.selectedImage + " " + styles.uploadedImage : styles.uploadedImage}
+            onClick={() => setSelectedFile(image)}
+          />
+        ))}
+        {selectedFile && <button onClick={getContour}>Get Contour</button>}
+        <h2>Contoured Images</h2>
+        {contouredImages.length > 0 && contouredImages.map((image) => (
+          <Image
+            key={image}
+            src={`/opencv_store/${image}`}
+            alt="Uploaded Image"
+            width={200}
+            height={200}
+            className={selectedContouredFile === image ? styles.selectedContouredImage + " " + styles.contouredImage : styles.contouredImage}
+            onClick={() => setSelectedContouredFile(image)}
+          />
+        ))}
+        {responseData.message && <p>{responseData.message}</p>}
+        {responseData.fileName && <p>{responseData.fileName}</p>}
+        <h2>Upload an Image</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="file" onChange={handleFileChange} />
+          <button type="submit">Upload</button>
+        </form>
+      </div>
+      <div className={styles.workBench}>
+        <div className={styles.workPiece}>
+          {selectedContouredFile && (
+            <Image
+              src={`/opencv_store/${selectedContouredFile}`}
+              alt="Contoured Image"
+              width={400}
+              height={400}
+            />
+          )}
+        </div>
+      </div>
     </main>
   );
 }
