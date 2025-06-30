879fcf672a698adcc3bd0baa13d4ef73ef8aa7b3
Sophie Wirth <a.sophiewirth@gmail.com>
2025-06-30 14:11:40 +0200

    contour in meta

--- api/image_processing.py
+++ api/image_processing.py
@@ -14,26 +14,18 @@ def IP_create_contour_from_points(image):
     image_meta = json.loads(image)
     file = image_meta["fullPath"]
     cv_image = cv2.imread(file)
-    pprint('##################################################')
-    pprint(cv_image.shape)
     bounding_box_image = prepare_image(file, "opencv_store", "_bb")
     jsonFile = PATH_TO_IMAGE_PROCESSING_META
     with open(jsonFile, "r") as file:
         processing_meta = json.load(file)
-    
-    pprint('##################################################')
-    pprint(processing_meta[str(image_meta["id"])])
-    pprint('##################################################')
     tl = [processing_meta[str(image_meta["id"])]["topLeft"]["x"], processing_meta[str(image_meta["id"])]["topLeft"]["y"]]
     tr = [processing_meta[str(image_meta["id"])]["topRight"]["x"], processing_meta[str(image_meta["id"])]["topRight"]["y"]]
     br = [processing_meta[str(image_meta["id"])]["bottomRight"]["x"], processing_meta[str(image_meta["id"])]["bottomRight"]["y"]]
     bl = [processing_meta[str(image_meta["id"])]["bottomLeft"]["x"], processing_meta[str(image_meta["id"])]["bottomLeft"]["y"]]
-    
-    pprint('#############_______________________############')
     contour = [np.array([tl, tr, br, bl])]
-    pprint(contour)
-    pprint(bounding_box_image)
-    pprint('#############_______________________############')
     cv2.drawContours(cv_image, contour, 0, (0, 255, 0), 2)
     cv2.imwrite(bounding_box_image, cv_image)
-    
+    new_values = {
+        "contouredImage": bounding_box_image
+    }
+    update_processing_meta(image, json.dumps(new_values), jsonFile)
